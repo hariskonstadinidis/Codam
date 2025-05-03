d5eb3c77b87280ffe24c1b293443339996d7f824
hkonstan <haris_konstadinidis@hotmail.com>
2025-05-03 15:27:09 +0300

    ft_strchr fix

--- Projects/Libft/ft_strchr.c
+++ Projects/Libft/ft_strchr.c
@@ -6,10 +6,11 @@
 /*   By: hariskon <hariskon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 15:39:39 by hariskon          #+#    #+#             */
-/*   Updated: 2025/05/02 17:50:15 by hariskon         ###   ########.fr       */
+/*   Updated: 2025/05/03 15:24:38 by hariskon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
 #include "libft.h"
 
 char	*ft_strchr(char *letter, int a)
@@ -17,13 +18,11 @@ char	*ft_strchr(char *letter, int a)
 	while (*letter)
 	{
 		if (*letter == a)
-		{
-			return (letter);
-		}
+			return ((char *)letter);
 		else
-		{
 			letter++;
-		}
 	}
-	return (letter);
+	if (a == '\0')
+		return (letter);
+	return (NULL);
 }
